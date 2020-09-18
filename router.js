45a764c68b17e87985e7c62bb32e207289943068
Arnel Lenteria <arnel.lenteria@gmail.com>
2020-09-18 17:47:09 +0800

    cleanup

--- router.js
+++ router.js
@@ -1,11 +1,12 @@
 'use strict'
 
 var core = require('../core')
-var { router } = core
+var { router, middlewares } = core
+var { express, bodyParser } = middlewares
 var terminal_ctrl = require('./controllers/terminal_ctrl')
 
 router.get('/terminal-plugin', terminal_ctrl.get)
-router.post('/terminal-plugin/run-command', terminal_ctrl.runCommand)
-router.post('/terminal-plugin/abort-commands', terminal_ctrl.abortCommands)
+router.post('/terminal-plugin/run-command', express.urlencoded({ extended: true }), bodyParser.json(), terminal_ctrl.runCommand)
+router.post('/terminal-plugin/abort-commands', express.urlencoded({ extended: true }), bodyParser.json(), terminal_ctrl.abortCommands)
 
 module.exports = router
